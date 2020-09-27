1e8d021afb70fade4ae7f255de0e8522733aeae5
Max <priymak615@gmail.com>
2020-09-27 20:54:14 +0300

    Update: add product page

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,92 +4,96 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const routerConfig = [
-    {
-        name: 'index',
-        template: 'index'
-    },
-    {
-        name: 'test',
-        template: 'ui'
-    }
+  {
+    name: 'index',
+    template: 'index'
+  },
+  {
+    name: 'product',
+    template: 'product'
+  },
+  {
+    name: 'test',
+    template: 'ui'
+  }
 ];
 
 
 module.exports = {
-    entry: {
-        app: Path.resolve(__dirname, '../src/scripts/index.js')
-    },
-    output: {
-        path: Path.join(__dirname, '../build'),
-        filename: 'js/[name].js'
-    },
-/*    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            name: false
+  entry: {
+    app: Path.resolve(__dirname, '../src/scripts/index.js')
+  },
+  output: {
+    path: Path.join(__dirname, '../build'),
+    filename: 'js/[name].js'
+  },
+  /*    optimization: {
+          splitChunks: {
+              chunks: 'all',
+              name: false
+          }
+      },*/
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {from: Path.resolve(__dirname, '../static'), to: 'static'}
+    ]),
+    ...routerConfig.reduce((routing, routeConfig) => {
+      let route = new HtmlWebpackPlugin({
+        filename: `${routeConfig.name}.html`,
+        template: Path.resolve(__dirname, `../src/templates/${routeConfig.template}.njk`),
+        minify: {
+          collapseWhitespace: false
         }
-    },*/
-    plugins: [
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin([
-            {from: Path.resolve(__dirname, '../static'), to: 'static'}
-        ]),
-        ...routerConfig.reduce((routing, routeConfig) => {
-            let route = new HtmlWebpackPlugin({
-                filename: `${routeConfig.name}.html`,
-                template: Path.resolve(__dirname, `../src/templates/${routeConfig.template}.njk`),
-                minify: {
-                    collapseWhitespace: false
-                }
-            });
+      });
 
-            routing.push(route);
+      routing.push(route);
 
-            return routing;
-        }, [])
-    ],
-    resolve: {
-        alias: {
-            '~': Path.resolve(__dirname, '../src'),
-            '@': Path.resolve(__dirname, '../'),
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.njk$/,
-                use: [
-                    {
-                        loader: 'simple-nunjucks-loader',
-                        options: {
-                            searchPaths: [
-                                './src/templates/'
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.mjs$/,
-                include: /node_modules/,
-                type: 'javascript/auto'
-            },
-            {
-                test: /\.svg$/,
-                use: [
-                    'svg-sprite-loader',
-                    'svgo-loader'
-                ]
-            },
-            {
-                test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[path][name].[ext]'
-                    }
-                }
-            },
-        ]
+      return routing;
+    }, [])
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../src'),
+      '@': Path.resolve(__dirname, '../'),
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.njk$/,
+        use: [
+          {
+            loader: 'simple-nunjucks-loader',
+            options: {
+              searchPaths: [
+                './src/templates/'
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          'svg-sprite-loader',
+          'svgo-loader'
+        ]
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }
+      },
+    ]
+  }
 };
